c5395e804bddaaa0245ef240a9d7716d557bfb64
Tian Khamez <tkhamez@gmail.com>
2022-01-01 13:58:26 +0100

    Add previous_character_id database table column.

--- slack_signup.sql
+++ slack_signup.sql
@@ -1,14 +1,15 @@
 
-CREATE TABLE `invite`
+create table invite
 (
-    `character_id`   int(11) NOT NULL,
-    `character_name` text    NOT NULL,
-    `email`          text    NOT NULL,
-    `email_history`  text DEFAULT NULL,
-    `invited_at`     int(11) NOT NULL,
-    `slack_id`       text DEFAULT NULL,
-    `account_status` text DEFAULT NULL,
-    `slack_name`     varchar(1024) DEFAULT NULL,
-    PRIMARY KEY (`character_id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+    character_id          int          not null
+        primary key,
+    character_name        text         not null,
+    email                 text         not null,
+    email_history         text         null,
+    invited_at            int          not null,
+    slack_id              text         null,
+    account_status        text         null,
+    slack_name            varchar(255) null,
+    previous_character_id int          null
+)
+    charset = utf8;
